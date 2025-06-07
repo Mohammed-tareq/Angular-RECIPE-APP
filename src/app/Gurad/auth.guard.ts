3657ff361e1b63494179ea3aecd35604f6e5ef1e
Eyad Osama <eyadosama0616@gmail.com>
2025-06-08 02:38:42 +0300

    handling auth and session management

--- src/app/Gurad/auth.guard.ts
+++ src/app/Gurad/auth.guard.ts
@@ -1,19 +1,36 @@
-import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, RouterStateSnapshot } from '@angular/router';
-import { AuthService } from '../servies/auth/auth.service';
+// import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, RouterStateSnapshot } from '@angular/router';
+// import { AuthService } from '../servies/auth/auth.service';
 
-export class AuthGurad implements CanActivate{
+// export class AuthGurad implements CanActivate{
 
-  constructor(private authService: AuthService) {}
+//   constructor(private authService: AuthService) {}
 
 
-  canActivate():boolean {
-    if(this.authService.isLoggedIn()) {
-      return true;
-    }else {
-      alert("You are not allowed to view this page, please login first");
-      return false;
-    }
+//   canActivate():boolean {
+//     if(this.authService.isLoggedIn()) {
+//       return true;
+//     }else {
+//       alert("You are not allowed to view this page, please login first");
+//       return false;
+//     }
 
     
+//   }
+// }
+
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../servies/auth/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (this.auth.isAuthenticated()) return true;
+    this.router.navigate(['/login']);
+    return false;
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
-import { Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
 import { RecipeSearchComponent } from './component/recipe-search/recipe-search.component';
+import { RegisterComponent } from './component/register/register.component';
 import { MainComponent } from './pages/home/main/main.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './Gurad/auth.guard'
 
 export const routes: Routes = [
-    {path: '', redirectTo: '/home', pathMatch: 'full' },
-    {path:'home', component: MainComponent },
-    {path: 'recipe-search', component: RecipeSearchComponent },
+    {path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    {path:'home', component: MainComponent, canActivate: [AuthGuard] },
+    {path: 'recipe-search', component: RecipeSearchComponent, canActivate: [AuthGuard] },
     {path: 'Recipes', loadComponent: () => import('./component/recipe-list/recipe-list.component').then(m => m.RecipeListComponent) },
+    {path: '**', redirectTo: 'login'},
 ];
