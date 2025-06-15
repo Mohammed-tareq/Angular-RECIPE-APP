7d9199871f418d547b46be64498c7e3973764a82
Mohammed tareq <Mohtareq1999m@gmail.com>
2025-06-15 21:50:11 +0300

    refactor: clean up item-for-search component and enhance single-item image styling

--- src/app/component/item-for-search/item-for-search.component.ts
+++ src/app/component/item-for-search/item-for-search.component.ts
@@ -1,33 +1,41 @@
 import { Component } from '@angular/core';
 import { SpoonacularService } from '../../servies/spoonacular/spoonacular.service';
 import { CommonModule } from '@angular/common';
-import { Router   } from '@angular/router';
+import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-item-for-search',
-  imports: [CommonModule, RouterModule ],
+  imports: [CommonModule, RouterModule],
   templateUrl: './item-for-search.component.html',
   styleUrls: ['./item-for-search.component.scss']
 })
 export class ItemForSearchComponent {
 
   items: any[] = [];
-  // itemShow: any[] = [];
+  foods: string[] = ["Sushi", "Burger", "Salad", "Sushi", "Tacos"];
+  index: number = 0;
+  currentFood!: string;
 
-  constructor(private spoonSer: SpoonacularService , private route: Router) { }
+
+  constructor(private spoonSer: SpoonacularService, private route: Router) {}
+    
 
   ngOnInit(): void {
-  this.spoonSer.getRecipeStatic("salad").subscribe({
-    next: (data: any) => {
-      this.items = data.menuItems;
-      // this.itemShow = this.items.slice(0, 3); // Show only the first 10 items
-    },
-    error: (error: any) => {
-      this.route.navigate(['/404']);
-      console.error('Error fetching recipes:', error);
-    }
-  });
-}
+    this.spoonSer.getRecipeStatic("salad").subscribe({
+      next: (data: any) => {
+        this.items = data.menuItems;
+        // this.itemShow = this.items.slice(0, 3); // Show only the first 10 items
+      },
+      error: (error: any) => {
+        this.route.navigate(['/404']);
+        console.error('Error fetching recipes:', error);
+      }
+    });
+  }
+
+  
+
+ 
 
 }
 
